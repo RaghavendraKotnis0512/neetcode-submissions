8579040d38d2333f391e894579fae682084bca09
Raghavendra Kotnis <134831700+RaghavendraKotnis0512@users.noreply.github.com>
2026-05-22 10:25:50 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>ans(nums.size(),1);
+
+        int prefix=1;
+        for(int i=0;i<nums.size();i++){
+            ans[i]*=prefix;
+            prefix*=nums[i];
+        }
+        int suffix=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return ans;
+
+        
+
+    // 1    2   4   6
+    // 1    2   8   32
+    // 48    48    24    6
+
+    }
+};
