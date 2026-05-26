cc85cf2449d7de34b05aaf1468e6c5354edbbf56
Raghavendra Kotnis <134831700+RaghavendraKotnis0512@users.noreply.github.com>
2026-05-26 22:56:15 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int i=0;
+        while(i<s.length()){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()) return false;
+                if((st.top()=='(' && s[i]==')') || 
+            (st.top()=='{' && s[i]=='}') ||
+            (st.top()=='[' && s[i]==']')){
+                st.pop();
+            }else{
+                return false;
+            }
+            }
+            
+
+            i++;
+        }
+        return st.empty();
+    }
+};
